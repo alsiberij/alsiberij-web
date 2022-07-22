00e2d2f1a6d89a86cb68d062af498a7999af3d11
Alexander Sibirtsev <alsiberij@gmail.com>
2022-07-22 14:58:11 +0300

    Alsiberij-main (GO) temporarily removed

--- alsiberij-main/server/responses.go
+++
@@ -1,9 +0,0 @@
-package server
-
-const (
-	ContentTypeCss  = "text/css"
-	ContentTypeHtml = "text/html"
-	ContentTypeJson = "application/json"
-	ContentTypeIcon = "image/x-icon"
-	ContentTypePng  = "image/png"
-)
