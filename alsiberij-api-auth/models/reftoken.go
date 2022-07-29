9f310e35fef86fc3ce020cc774b5972e25750154
Alexander Sibirtsev <alsiberij@gmail.com>
2022-07-29 12:23:06 +0300

    "Alsiberij-api-auth" Nullable types added

--- alsiberij-api-auth/models/reftoken.go
+++ alsiberij-api-auth/models/reftoken.go
@@ -6,12 +6,12 @@ import (
 
 type (
 	RefreshToken struct {
-		Id         int64      `json:"id"`
-		User       User       `json:"user"`
-		Token      string     `json:"token"`
-		IsExpired  bool       `json:"isExpired"`
-		ExpiresAt  time.Time  `json:"expiresAt"`
-		IssuedAt   time.Time  `json:"issuedAt"`
-		LastUsedAt *time.Time `json:"lastUsed"` //TODO MAKE NULLABLE TYPE
+		Id         int64     `json:"id"`
+		User       User      `json:"user"`
+		Token      string    `json:"token"`
+		IsExpired  bool      `json:"isExpired"`
+		ExpiresAt  time.Time `json:"expiresAt"`
+		IssuedAt   time.Time `json:"issuedAt"`
+		LastUsedAt NullTime  `json:"lastUsed"`
 	}
 )
