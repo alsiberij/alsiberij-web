594439770d42fc924bca86fee33007a39604e737
Alexander Sibirtsev <alsiberij@gmail.com>
2022-08-05 16:49:45 +0300

    API-GO-AUTH Prefix /api/v1 added to all routes

--- api-go-auth/main.go
+++ api-go-auth/main.go
@@ -11,6 +11,10 @@ import (
 	"os"
 )
 
+const (
+	ApiV1 = "/api/v1"
+)
+
 func init() {
 	config, err := ReadConfig("config.json")
 	if err != nil {
@@ -53,31 +57,31 @@ func main() {
 	r.MethodNotAllowed = srv.Set405
 	r.PanicHandler = srv.Set500
 
-	r.GET("/", fasthttp.RequestHandler(
+	r.GET(ApiV1+"/", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.Test, srv.AddExecutionTimeHeader)))
 
-	r.POST("/login", fasthttp.RequestHandler(
+	r.POST(ApiV1+"/login", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.Login, srv.AddExecutionTimeHeader)))
 
-	r.POST("/refresh", fasthttp.RequestHandler(
+	r.POST(ApiV1+"/refresh", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.Refresh, srv.AddExecutionTimeHeader)))
 
-	r.DELETE("/refresh", fasthttp.RequestHandler(
+	r.DELETE(ApiV1+"/refresh", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.Revoke, srv.AddExecutionTimeHeader)))
 
-	r.POST("/checkEmail", fasthttp.RequestHandler(
+	r.POST(ApiV1+"/checkEmail", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.CheckEmail, srv.AddExecutionTimeHeader)))
 
-	r.POST("/register", fasthttp.RequestHandler(
+	r.POST(ApiV1+"/register", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.Register, srv.AddExecutionTimeHeader)))
 
-	r.GET("/validateJWT", fasthttp.RequestHandler(
+	r.GET(ApiV1+"/validateJWT", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.ValidateJWT, srv.Authorize, srv.AddExecutionTimeHeader)))
 
-	r.GET("/users", fasthttp.RequestHandler(
+	r.GET(ApiV1+"/users", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.Users, srv.AuthorizeRoles([]string{jwt.RoleCreator, jwt.RoleAdmin, jwt.RoleModerator}), srv.AddExecutionTimeHeader)))
 
-	r.PATCH("/user/{id}/status", fasthttp.RequestHandler(
+	r.PATCH(ApiV1+"/user/{id}/status", fasthttp.RequestHandler(
 		srv.WithMiddlewares(srv.ChangeUserStatus, srv.AuthorizeRoles([]string{jwt.RoleCreator, jwt.RoleAdmin, jwt.RoleModerator}), srv.AddExecutionTimeHeader)))
 
 	s := fasthttp.Server{
