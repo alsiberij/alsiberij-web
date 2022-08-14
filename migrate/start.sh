7e8843490a8933f557a6b16f184317e6ea9e6eee
Alexander Sibirtsev <alsiberij@gmail.com>
2022-08-14 20:57:35 +0300

    POSTGRES-1 Service renamed

--- migrate/start.sh
+++ migrate/start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 echo "START...";
-
-if [[ $1 != "up" && $1 != "down" ]];then
-  echo "ONLY UP/DOWN ALLOWED IN FIRST ARG"
-  exit 1
-fi
-
 echo "MIGRATING AUTH";
-migrate -database postgres://"$PG_AUTH_USER":"$PG_AUTH_USER"@pgs:5432/"$PG_DB"?sslmode=disable -path /migrations/alsiberij-api-auth/ "$1" "$2";
-echo "DONE";
+migrate -database postgres://"$1":"$2"@"$PG_AUTH_HOST":"$PG_AUTH_PORT"/"$PG_AUTH_DB"?sslmode=disable -path /migrations/alsiberij-api-auth/ "$3" "$4" && echo "DONE";
