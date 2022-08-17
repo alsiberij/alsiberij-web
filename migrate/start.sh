b101f5bf0886558afad52db4dc9e380c08d877bc
Alexander Sibirtsev <alsiberij@gmail.com>
2022-08-17 11:54:27 +0300

    MIGRATE Migration path fixed

--- migrate/start.sh
+++ migrate/start.sh
@@ -2,4 +2,4 @@
 
 echo "START...";
 echo "MIGRATING AUTH";
-migrate -database postgres://"$1":"$2"@"$PG_AUTH_HOST":"$PG_AUTH_PORT"/"$PG_AUTH_DB"?sslmode=disable -path /migrations/alsiberij-api-auth/ "$3" "$4" && echo "DONE";
+migrate -database postgres://"$1":"$2"@"$PG_AUTH_HOST":"$PG_AUTH_PORT"/"$PG_AUTH_DB"?sslmode=disable -path /migrations/api-go-auth/ "$3" "$4" && echo "DONE";
