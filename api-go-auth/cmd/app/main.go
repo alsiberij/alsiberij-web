ce4be85b928540e82090d4dcd8dee75512652774
Alexander Sibirtsev <alsiberij@gmail.com>
2022-09-30 11:51:18 +0300

    API-GO-AUTH App minor refactor

--- api-go-auth/cmd/app/main.go
+++ api-go-auth/cmd/app/main.go
@@ -6,10 +6,31 @@ import (
 	"auth/pkg/pgs"
 	"auth/pkg/rds"
 	"crypto/tls"
+	"encoding/json"
 	"log"
 	"os"
 	"time"
 )
+
+type (
+	Config struct {
+		PgsAuth pgs.PostgresConfig `json:"pgs-1/auth"`
+		Rds0    rds.RedisConfig    `json:"rds-1/0"`
+		Rds1    rds.RedisConfig    `json:"rds-1/1"`
+	}
+)
+
+func ReadConfig(filename string) (Config, error) {
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0444)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	err = json.NewDecoder(f).Decode(&config)
+	_ = f.Close()
+	return config, err
+}
 
 func main() {
 	config, err := ReadConfig("./config.json")
@@ -71,5 +92,5 @@ func main() {
 
 	srv.Serve()
 
-	log.Printf("BYE...")
+	log.Printf("BYE...\n")
 }
